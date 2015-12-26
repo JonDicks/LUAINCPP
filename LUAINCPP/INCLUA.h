182428484881c277bb23fb8d1bbd16da0cd3701f
PandaWithAShotgun <dicksjonathon@gmail.com>
2015-12-26 11:55:02 -0500

    Set up the inclusion of LUA. Does not yet have the LUA stack set up or any of the environment variables.

---
+++ LUAINCPP/INCLUA.h
@@ -0,0 +1,18 @@
+//
+//  INCLUA.h
+//  LUAINCPP
+//
+//  Created by Jon Dicks on 12/26/15.
+//  Copyright © 2015 Jon Dicks. All rights reserved.
+//
+
+#ifndef INCLUA_h
+#define INCLUA_h
+
+extern "C"{
+    #include </usr/local/include/lua.h>
+    #include </usr/local/include/lualib.h>
+    #include </usr/local/include/lauxlib.h>
+}
+
+#endif /* INCLUA_h */
--- LUAINCPP/main.cpp
+++ LUAINCPP/main.cpp
@@ -7,6 +7,9 @@
 //
 
 #include <iostream>
+#include "INCLUA.h"
+
+
 
 int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
