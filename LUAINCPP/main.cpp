53b9e36e20a6737bea32969d99ac6b3af80dc751
PandaWithAShotgun <dicksjonathon@gmail.com>
2015-12-26 13:19:11 -0500

    *Updated the code to have a void that allocates space for the stack and added a newstate clause to the LUA state.

--- LUAINCPP/main.cpp
+++ LUAINCPP/main.cpp
@@ -9,9 +9,19 @@
 #include <iostream>
 #include "INCLUA.h"
 
-
+static void *l_alloc (void *ud, void *ptr, size_t osize, size_t nsize){
+    (void) ud;
+    (void) osize;
+    if (nsize == 0){
+        free(ptr);
+        return NULL;
+    }
+    else  return realloc(ptr, nsize);
+}
 
 int main(int argc, const char * argv[]) {
+    lua_State *lua = luaL_newstate();
+    std:: cout << lua;
     std::cout << "Hello, World!\n";
     return 0;
 }
